e6a401f6ba74276a8b219206997e919c0f75d3d7
Musafaizan <iammusa182@gmail.com>
2026-03-25 15:23:24 +0500

    whatsapp

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import WhatsAppButton from "./components/WhatsAppButton";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -21,6 +22,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         {children}
+        <WhatsAppButton />
       </body>
     </html>
   );
